00934d19ab56f2fd78ef96f9cdfd31153f9b3cb4
odys zhou <odysseusj@163.com>
2019-02-26 14:59:08 +0800

    initing

--- jserv-sample/src/main/java/io/odysz/jsample/SampleCase.java
+++ jserv-sample/src/main/java/io/odysz/jsample/SampleCase.java
@@ -1,5 +1,8 @@
 package io.odysz.jsample;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
 @WebServlet(description = "querying db via Semantic.DA", urlPatterns = { "/case.sample" })
 public class SampleCase  extends HttpServlet {
 
