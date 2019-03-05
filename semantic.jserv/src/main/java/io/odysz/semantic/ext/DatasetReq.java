e377cef4133412aea00f485f4c76d070af08546d
odysz <odysseusj@163.com>
2019-03-06 01:02:25 +0800

    testing DatasetReq

--- semantic.jserv/src/main/java/io/odysz/semantic/ext/DatasetReq.java
+++ semantic.jserv/src/main/java/io/odysz/semantic/ext/DatasetReq.java
@@ -1,7 +1,15 @@
 package io.odysz.semantic.ext;
 
+import java.io.IOException;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import io.odysz.common.Utils;
 import io.odysz.semantic.DA.DatasetCfg.TreeSemantics;
 import io.odysz.semantic.jprotocol.JBody;
+import io.odysz.semantic.jprotocol.JHelper;
 import io.odysz.semantic.jprotocol.JMessage;
 import io.odysz.semantic.jserv.R.QueryReq;
 
@@ -41,6 +49,67 @@ public class DatasetReq extends QueryReq {
 
 	public void treeSemtcs(TreeSemantics semtcs) {
 		this.stcs = semtcs;
+	}
+
+	@Override
+	public void toJson(JsonWriter writer) throws IOException {
+		writer.beginObject();
+		writer.name("a").value(a);
+		writer.name("sk").value(sk);
+		writer.name("rootId").value(rootId);
+		writer.name("page").value(page);
+		writer.name("pgSize").value(pgsize);
+
+		writer.name("smtcss").value(stcs == null ? "[" + smtcss + "]" : stcs.toJson());
+
+		try {
+			if (sqlArgs != null) {
+				writer.name("joins");
+				JHelper.writeStrings(writer, sqlArgs);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();	
+		}
+		writer.endObject();
+	}
+
+	@Override
+	public void fromJson(JsonReader reader) throws IOException {
+		JsonToken token = reader.peek();
+		if (token == JsonToken.BEGIN_OBJECT) {
+			reader.beginObject();
+			while (token != JsonToken.END_OBJECT) {
+				String name = reader.nextName();
+				if ("a".equals(name))
+					a = nextString(reader);
+				else if ("page".equals(name))
+					page = reader.nextInt();
+				else if ("pgSize".equals(name))
+					pgsize = reader.nextInt();
+				else if ("sk".equals(name))
+					sk = nextString(reader);
+				else if ("rootId".equals(name))
+					rootId = nextString(reader);
+				else if ("smtcss".equals(name)) {
+					JsonToken peek = reader.peek();
+					if (peek == JsonToken.BEGIN_ARRAY) {
+						String[] ss = JHelper.readStrs(reader);
+						stcs = new TreeSemantics(ss);
+					}
+					else if (peek == JsonToken.NULL)
+						reader.nextNull();
+					else {
+						// What's this?
+						Utils.warn("Not handled: %s - expecting array", reader.nextString());
+					}
+				}
+				else if ("sqlArgs".equals(name))
+					sqlArgs = JHelper.readStrs(reader);
+
+				token = reader.peek();
+			}
+			reader.endObject();
+		}
 	}
 
 }
