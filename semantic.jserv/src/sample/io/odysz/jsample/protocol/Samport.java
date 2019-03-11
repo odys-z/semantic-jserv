c1f3305dd01129e93a0dda74345881cca4721f99
odys zhou <odysseusj@163.com>
2019-03-11 15:30:45 +0800

    vue sidebar menu is basically working

--- semantic.jserv/src/sample/io/odysz/jsample/protocol/Samport.java
+++ semantic.jserv/src/sample/io/odysz/jsample/protocol/Samport.java
@@ -2,6 +2,7 @@ package io.odysz.jsample.protocol;
 
 import io.odysz.semantic.jprotocol.IPort;
 import io.odysz.semantic.jprotocol.JMessage.Port;
+import io.odysz.semantics.x.SemanticException;
 
 public enum Samport implements IPort {
 	menu("menu.sample"); 
@@ -10,12 +11,16 @@ public enum Samport implements IPort {
 	Samport(String v) { url = v; };
 	public String url() { return url; }
 	@Override
-	public IPort valof(String pname) {
+	public IPort valof(String pname) throws SemanticException {
 		try {
 			IPort p = Port.valueOf(pname);
 			return p;
 		} catch (Exception e) {
-			return valueOf(pname);
+			try { return valueOf(pname); }
+			catch (IllegalArgumentException ex) {
+				throw new SemanticException(ex.getMessage());
+			}
+			
 		}
 	}	
 }
