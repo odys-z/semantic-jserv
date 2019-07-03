de6730c203a5a60acc0d73a5e2e8d70cd52e6ecd
odys zhou <odysseusj@163.com>
2019-07-03 20:23:41 +0800

    upgarding new version for bugs of jsample

---
+++ jserv-sample/src/main/java/io/odysz/jsample/SampleUser.java
@@ -0,0 +1,33 @@
+package io.odysz.jsample;
+
+import io.odysz.semantic.jsession.JUser;
+import io.odysz.semantics.meta.TableMeta;
+import io.odysz.semantics.x.SemanticException;
+
+public class SampleUser extends JUser {
+	/**Hard coded field string of user table information.
+	 * With this class, sample project's user table can be different from the default table,
+	 * providing the same semantics presented.
+	 * @author odys-z@github.com
+	 */
+	public static class SampleUserMeta extends JUserMeta {
+		public SampleUserMeta(String tbl, String... conn) {
+			super(tbl, conn);
+
+			this.tbl = "a_users";
+			pk = "userId";
+			uname = "userName";
+			pswd = "pswd";
+			iv = "iv";
+		}
+	}
+
+	public SampleUser(String uid, String pswd, String usrName) throws SemanticException {
+		super(uid, pswd, usrName);
+	}
+
+	public TableMeta meta() {
+		return new SampleUserMeta("");
+	}
+
+}
