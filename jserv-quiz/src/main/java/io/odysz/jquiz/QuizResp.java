8c88ee199d0bd29cd7cd8ec6f41cb112601165fc
Odys Zhou <odys.zhou@gmail.com>
2021-06-10 01:16:00 +0800

    .

--- jserv-quiz/src/main/java/io/odysz/jquiz/QuizResp.java
+++ jserv-quiz/src/main/java/io/odysz/jquiz/QuizResp.java
@@ -1,13 +1,37 @@
 package io.odysz.jquiz;
 
 import io.odysz.semantic.jprotocol.AnsonResp;
+import io.odysz.semantics.SemanticObject;
 
 public class QuizResp extends AnsonResp {
 
-	public QuizResp() { }
+	protected SemanticObject data;
+
+	public QuizResp() {
+		data = new SemanticObject();
+	}
+
+	public QuizResp(SemanticObject datium) {
+		data = datium;
+	}
 
 	public QuizResp msg(String string) {
 		m = string;
+		return this;
+	}
+	
+	public QuizResp quizId(String qid) {
+		data.put(QuizProtocol.quizId, qid);
+		return this;
+	}
+	
+	public QuizResp title(String title) {
+		data.put(QuizProtocol.qtitle, title);
+		return this;
+	}
+	
+	public QuizResp questions(int ques) {
+		data.put(QuizProtocol.questions, ques);
 		return this;
 	}
 
