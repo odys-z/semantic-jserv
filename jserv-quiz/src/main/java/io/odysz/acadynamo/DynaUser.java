6ef5485157458e0f4d0750de50243c32ae13269d
odys zhou <odysseusj@163.com>
2021-08-02 17:09:46 +0800

    extending jserv for North...

--- jserv-quiz/src/main/java/io/odysz/acadynamo/DynaUser.java
+++ jserv-quiz/src/main/java/io/odysz/acadynamo/DynaUser.java
@@ -1,11 +1,11 @@
-package io.odysz.jquiz.app;
+package io.odysz.acadynamo;
 
 import io.odysz.common.LangExt;
 import io.odysz.semantic.jsession.JUser;
 import io.odysz.semantics.meta.TableMeta;
 import io.odysz.semantics.x.SemanticException;
 
-public class QuizUser extends JUser {
+public class DynaUser extends JUser {
 	/**Hard coded field string of user table's information.
 	 * With this class, the project's user table can be different from the default table,
 	 * providing the same semantics presented.
@@ -23,7 +23,7 @@ public class QuizUser extends JUser {
 		}
 	}
 
-	public QuizUser(String uid, String pswd, String usrName) throws SemanticException {
+	public DynaUser(String uid, String pswd, String usrName) throws SemanticException {
 		super(uid, pswd, usrName);
 	}
 
