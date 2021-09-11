714bf44f6445325f0a8b433227c14a4bde5ca3be
odys zhou <odysseusj@163.com>
2021-09-11 18:20:21 +0800

    ..

--- jserv-sample/src/main/java/io/oz/ever/conn/n/gpa/GPAResq.java
+++
@@ -1,8 +0,0 @@
-package io.oz.ever.conn.n.gpa;
-
-import io.odysz.semantic.jprotocol.AnsonMsg;
-import io.odysz.semantic.jprotocol.AnsonResp;
-
-public class GPAResq extends AnsonMsg<AnsonResp> {
-
-}
