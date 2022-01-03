535fedc99963c6c8460dcfed9a87fcc90ed84cae
Odys Zhou <odys.zhou@gmail.com>
2022-01-03 17:55:50 -0500

    add jserv-album: mysql docker running

---
+++ jserv-album/src/main/java/io/oz/album/AlbumSingleton.java
@@ -0,0 +1,26 @@
+package io.oz.album;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import io.odysz.semantic.jprotocol.AnsonMsg;
+import io.odysz.semantic.jserv.JSingleton;
+
+@WebListener
+public class AlbumSingleton extends JSingleton implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		super.onInitialized(sce);
+		
+		AnsonMsg.understandPorts(AlbumPort.album);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		super.onDestroyed(sce);
+	}
+
+}
