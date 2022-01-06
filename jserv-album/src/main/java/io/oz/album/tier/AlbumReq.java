02ea9d5b3536f0cea024aaa6d46dec238797b811
Odys Zhou <odys.zhou@gmail.com>
2022-01-05 23:51:22 -0500

    .

---
+++ jserv-album/src/main/java/io/oz/album/tier/AlbumReq.java
@@ -0,0 +1,29 @@
+package io.oz.album.tier;
+
+import io.odysz.semantic.jprotocol.AnsonBody;
+import io.odysz.semantic.jprotocol.AnsonMsg;
+
+public class AlbumReq extends AnsonBody {
+	static class A {
+		static final String records = "r/collects";
+		static final String collect = "r/photos";
+		static final String rec = "r/photo";
+		static final String update = "u";
+		static final String insert = "c";
+		static final String del = "d";
+	}
+	
+	static class args {
+	}
+	
+	String aid;
+	
+	public AlbumReq() {
+		super(null, null);
+	}
+
+	protected AlbumReq(AnsonMsg<? extends AnsonBody> parent, String uri) {
+		super(parent, uri);
+	}
+
+}
