20f3bfb7b6832d61d4a9b34d855edbc4861a8039
odys zhou <odysseusj@163.com>
2022-05-20 17:17:39 +0800

    .

--- jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetResp.java
+++ jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetResp.java
@@ -1,13 +1,19 @@
 package io.oz.sandbox.sheet;
 
+import io.odysz.module.rs.AnResultset;
 import io.odysz.semantic.jprotocol.AnsonResp;
 
 public class SpreadsheetResp extends AnsonResp {
 
 	MyCurriculum rec;
 
+	AnResultset sheet;
+
 	public SpreadsheetResp(MyCurriculum rec) {
 		this.rec = rec;
 	}
 
+	public SpreadsheetResp(AnResultset sheet) {
+		this.sheet = sheet;
+	}
 }
