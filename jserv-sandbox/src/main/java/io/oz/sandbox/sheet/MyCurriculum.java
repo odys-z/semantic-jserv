83ad6fa85761c01bee756969bdd8173e742ef9db
odys zhou <odysseusj@163.com>
2022-05-23 19:39:35 +0800

    .

--- jserv-sandbox/src/main/java/io/oz/sandbox/sheet/MyCurriculum.java
+++ jserv-sandbox/src/main/java/io/oz/sandbox/sheet/MyCurriculum.java
@@ -3,6 +3,9 @@ package io.oz.sandbox.sheet;
 import io.odysz.anson.Anson;
 
 public class MyCurriculum extends Anson {
+
+	public static final String tabl = "b_curriculums";
+
     String cid;
     String cate;
     String module;
--- jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetReq.java
+++ jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetReq.java
@@ -2,6 +2,9 @@ package io.oz.sandbox.sheet;
 
 import io.odysz.semantic.jprotocol.AnsonBody;
 import io.odysz.semantic.jprotocol.AnsonMsg;
+import io.odysz.semantic.jserv.R.PageInf;
+import io.odysz.transact.sql.Insert;
+import io.odysz.transact.x.TransException;
 
 public class SpreadsheetReq extends AnsonBody {
 	static class A {
@@ -11,14 +14,38 @@ public class SpreadsheetReq extends AnsonBody {
 		public static final String delete = "d";
 	}
 
-	public MyCurriculum rec;
+	MyCurriculum rec;
+	PageInf page;
 
-	protected SpreadsheetReq() {
+	public SpreadsheetReq() {
 		super(null, null);
 	}
 
 	protected SpreadsheetReq(AnsonMsg<? extends AnsonBody> parent, String uri) {
 		super(parent, uri);
+	}
+
+	public SpreadsheetReq insertRec(Insert inst) throws TransException {
+		
+		if (rec == null) {
+			rec = new MyCurriculum();
+			rec.currName = "curr name";
+			rec.cate = "cate";
+			rec.level = "level";
+			rec.subject = "subject";
+			rec.module = "module";
+			rec.sort = "999";
+		}
+		// else - default value by client
+
+		inst.nv("currName", rec.currName)
+			.nv("cate", rec.cate)
+			.nv("clevel", rec.level)
+			.nv("subject", rec.subject)
+			.nv("module", rec.module)
+			.nv("sort", rec.sort);
+		
+		return this;
 	}
 
 }
