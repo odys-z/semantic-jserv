08509d33d97516789873e95f4ea9b2a1f725e574
odys zhou <odysseusj@163.com>
2022-06-16 17:29:39 +0800

    v1.4.8 extend SpreadsheetReq

--- jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetReq.java
+++ jserv-sandbox/src/main/java/io/oz/sandbox/sheet/SpreadsheetReq.java
@@ -1,58 +1,58 @@
-package io.oz.sandbox.sheet;
-
-import io.odysz.semantic.jprotocol.AnsonBody;
-import io.odysz.semantic.jprotocol.AnsonMsg;
-import io.odysz.semantic.jserv.R.PageInf;
-import io.odysz.semantic.jserv.user.UserReq;
-import io.odysz.transact.sql.Insert;
-import io.odysz.transact.x.TransException;
-
-/**
- * TODO This envolope will be changed to generic type once Antson support it.
- * 
- * @author odys-z@github.com
- *
- */
-public class SpreadsheetReq extends UserReq {
-	static class A {
-		public static final String records = "r";
-		public static final String insert = "c";
-		public static final String update = "u";
-		public static final String delete = "d";
-	}
-
-	MyCurriculum rec;
-	PageInf page;
-
-	public SpreadsheetReq() {
-		super(null, null);
-	}
-
-	protected SpreadsheetReq(AnsonMsg<? extends AnsonBody> parent, String uri) {
-		super(parent, uri);
-	}
-
-	public SpreadsheetReq insertRec(Insert inst) throws TransException {
-		
-		if (rec == null) {
-			rec = new MyCurriculum();
-			rec.currName = "курс";
-			rec.cate = "";
-			rec.clevel = "";
-			rec.subject = "";
-			rec.module = "";
-			rec.sort = "999";
-		}
-		// else - default value by client
-
-		inst.nv("currName", rec.currName)
-			.nv("cate", rec.cate)
-			.nv("clevel", rec.clevel)
-			.nv("subject", rec.subject)
-			.nv("module", rec.module)
-			.nv("sort", rec.sort);
-		
-		return this;
-	}
-
-}
+//package io.oz.sandbox.sheet;
+//
+//import io.odysz.semantic.jprotocol.AnsonBody;
+//import io.odysz.semantic.jprotocol.AnsonMsg;
+//import io.odysz.semantic.jserv.R.PageInf;
+//import io.odysz.semantic.jserv.user.UserReq;
+//import io.odysz.transact.sql.Insert;
+//import io.odysz.transact.x.TransException;
+//
+///**
+// * TODO This envolope will be changed to generic type once Antson support it.
+// * 
+// * @author odys-z@github.com
+// *
+// */
+//public class SpreadsheetReq extends UserReq {
+//	static class A {
+//		public static final String records = "r";
+//		public static final String insert = "c";
+//		public static final String update = "u";
+//		public static final String delete = "d";
+//	}
+//
+//	MyCurriculum rec;
+//	PageInf page;
+//
+//	public SpreadsheetReq() {
+//		super(null, null);
+//	}
+//
+//	protected SpreadsheetReq(AnsonMsg<? extends AnsonBody> parent, String uri) {
+//		super(parent, uri);
+//	}
+//
+//	public SpreadsheetReq insertRec(Insert inst) throws TransException {
+//		
+//		if (rec == null) {
+//			rec = new MyCurriculum();
+//			rec.currName = "курс";
+//			rec.cate = "";
+//			rec.clevel = "";
+//			rec.subject = "";
+//			rec.module = "";
+//			rec.sort = "999";
+//		}
+//		// else - default value by client
+//
+//		inst.nv("currName", rec.currName)
+//			.nv("cate", rec.cate)
+//			.nv("clevel", rec.clevel)
+//			.nv("subject", rec.subject)
+//			.nv("module", rec.module)
+//			.nv("sort", rec.sort);
+//		
+//		return this;
+//	}
+//
+//}
---
+++ semantic.jserv/src/main/java/io/oz/spreadsheet/SpreadsheetResp.java
@@ -0,0 +1,20 @@
+package io.oz.spreadsheet;
+
+import java.util.ArrayList;
+
+import io.odysz.module.rs.AnResultset;
+import io.odysz.semantic.jprotocol.AnsonResp;
+
+public class SpreadsheetResp extends AnsonResp {
+
+	ISheetRec rec;
+
+	public SpreadsheetResp(ISheetRec rec) {
+		this.rec = rec;
+	}
+
+	@SuppressWarnings("serial")
+	public SpreadsheetResp(AnResultset sheet) {
+		this.rs = new ArrayList<AnResultset>() { {add(sheet);} };
+	}
+}
