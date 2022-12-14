843a00743ac67dec1df0d95052e962f00b6763dc
odys zhou <odysseusj@163.com>
2022-12-14 16:59:04 +0800

    issue: how to have synodes manage information of clients' downloading?
    What about if a client download a file from hub then connected to prv node?
    And what happen if a prv disconnected to hub and user uploaded a file to
    hub then connect to prv node?

---
+++ semantic.jserv/src/main/java/io/odysz/semantic/tier/docs/IProfileResolver.java
@@ -0,0 +1,24 @@
+package io.odysz.semantic.tier.docs;
+
+import io.odysz.semantics.IUser;
+
+/**
+ * Resolving file's saving folder etc., e.g.<br>
+ * parse "yyyy_MM" as folder from file's sharing date or creating date;<br>
+ * or handle sharer on ending of chain push.<br>
+ * 
+ * @author odys-z@github.com
+ *
+ */
+public interface IProfileResolver {
+	String synodeFolder(DocsReq req, IUser usr);
+	
+	/**
+	 * Modify request on chain ended.
+	 * 
+	 * @param req
+	 * @param usr
+	 * @return req itself
+	 */
+	DocsReq onStartPush(DocsReq req, IUser usr);
+}
