ac0451e06eda54883aa0bf3d813ba74858efd0f9
odys zhou <odysseusj@163.com>
2023-02-15 16:58:58 +0800

    tag: v1.4.12-SNAPSHOT for extening table stamps ...

--- docsync.jserv/docsphinx/design/sync.ddl.sql
+++ docsync.jserv/docsphinx/design/sync.ddl.sql
@@ -88,7 +88,11 @@ create table syn_stamp
 	tabl    varchar2(64) NOT NULL, -- e.g. 'h_photos'
 	synode  varchar2(12) NOT NULL, -- fk-on-del
 	crud	char(1)      NOT NULL, -- 'R' is ignored; 'D' is actually triggered a cleaning task; 'D' -> 'C'; for 'U', only h_photos.sync & h_photos.clientpath is handled
-	recount INTEGER      NOT NULL, -- not correct if multiple updated and not used as Semantics.DA can't access context for commitment results.
+	
+	recount INTEGER,
+	-- not correct if multiple updated and not used as Semantics.DA can't access context for commitment results.
+	-- as photos are uploaded file by file, this is probably only useful for debugging.
+	
 	syncstamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
