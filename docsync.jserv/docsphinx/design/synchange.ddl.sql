0beeedb22d02e37f1bbb755aaf09ddbbf9de5b99
odys zhou <odysseusj@163.com>
2023-03-30 15:20:03 +0800

    .

--- docsync.jserv/docsphinx/design/synchange.ddl.sql
+++ docsync.jserv/docsphinx/design/synchange.ddl.sql
@@ -4,11 +4,12 @@ create table syn_seq
 (
 	tabl    varchar2(64) not null, -- e.g. 'h_photos'
 	synodee varchar2(12) not null, -- subscriber, fk-on-del, client id asked for sychronizing
-	crud	char(1)      not null, -- 'R' is ignored; 'D' is actually triggered a cleaning task; 'D' -> 'C'; for 'U', only h_photos.sync & h_photos.clientpath is handled
-
 	recount integer,
 	-- not correct if multiple updated and not used as Semantics.DA can't access context for commitment results.
 	-- as photos are uploaded file by file, this is probably only useful for debugging.
+	crud	char(1),
+	-- only last one is correct, and is not semantically used
+	-- 'R' is ignored; 'D' is actually triggered a cleaning task; 'D' -> 'C'; for 'U', only h_photos.sync & h_photos.clientpath is handled
 
 	synyquist   integer not null,
 	cleanyquist integer not null
@@ -17,12 +18,13 @@ create table syn_seq
 drop table if exists syn_change;
 create table syn_change (
 	tabl        varchar2(64) not null, -- e.g. 'h_photos'
-	recId       varchar2(12) not null, -- recId
+	recId       varchar2(12) not null, -- entity record Id
 	synodee     varchar2(12) not null, -- subscriber, fk-on-del, synode id device to finish cleaning task
 	synoder     varchar2(12) not null, -- publisher, fk-on-del, synode id for resource's PK
-	clientpath  text         not null, -- for h_photos.fullpath, or composed PK for resouce's id
+	clientpath  text         not null, -- for h_photos.fullpath, or composed PK for resouce's id, not null?
 	clientpath2 text,                  -- support max 3 fields of composed PK
-	flag        char(1)      not null, -- 'D' deleting, 'C' close (not exists),'R' rejected by device owner
+	crud        char(1)      not null, -- I/U/D
+	flag        char(1)      not null, -- 'D' deleting, 'C' close (not exists),'R'/'E' rejected (then erased) by device owner
 	synyquist   integer      not null  -- last Nyquist sequence number of synodee
 );
 
