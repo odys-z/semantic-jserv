9444e81d1446d07cfa709b87234168cd92342c24
odys zhou <odysseusj@163.com>
2023-04-03 19:15:35 +0800

    .

--- docsync.jserv/docsphinx/design/synchange.ddl.sql
+++ docsync.jserv/docsphinx/design/synchange.ddl.sql
@@ -23,7 +23,7 @@ create table syn_change (
 	-- synodee     varchar2(12) not null, -- subscriber, fk-on-del, synode id device to finish cleaning task
 	synoder     varchar2(12) not null, -- publisher, fk-on-del, synode id for resource's PK
 	clientpath  text         not null, -- for h_photos.fullpath, or composed PK for resouce's id, not null?
-	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any betther patterns?
+	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any better patterns?
 	-- synyquist   integer      not null  -- last Nyquist sequence number of synodee
 	crud        char(1)      not null  -- I/U/D
 	-- flag        char(1)      not null  -- 'D' deleting, 'C' close (not exists),'R'/'E' rejected (then erased) by device owner
@@ -34,7 +34,7 @@ create table syn_subscribe (
 	tabl        varchar2(64) not null, -- e.g. 'h_photos'
 	recId       varchar2(12) not null, -- entity record Id
 	clientpath  text         not null, -- for h_photos.fullpath, or composed PK for resouce's id, not null?
-	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any betther patterns?
+	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any better patterns?
 	synodee     varchar2(12) not null  -- subscriber, fk-on-del, synode id device to finish cleaning task
 	synyquist   integer      not null  -- last Nyquist sequence number of synodee
 );
@@ -44,7 +44,7 @@ create table syn_clean (
 	tabl        varchar2(64) not null, -- e.g. 'h_photos'
 	synoder     varchar2(12) not null, -- publisher, fk-on-del, synode id for resource's PK
 	clientpath  text         not null, -- for h_photos.fullpath, or composed PK for resouce's id
-	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any betther patterns?
+	clientpath2 text,                  -- support max 3 fields of composed PK, TODO any better patterns?
 	synodee     varchar2(12) not null, -- subscriber, fk-on-del, synode id device to finish cleaning task
 	flag        char(1)      not null, -- 'D' deleting, 'C' close (not exists),'R' rejected by device owner
 	cleanyquist integer      not null  -- last Nyquist sequence number of synodee
