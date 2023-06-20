89f3cded17cc73420af055e3ce5167f6e508eddd
odys zhou <odysseusj@163.com>
2023-06-20 20:16:52 +0800

    echo test results in escape error

--- jserv-sample/src/main/java/io/odysz/jsample/cheap/CheapReq.java
+++ jserv-sample/src/main/java/io/odysz/jsample/cheap/CheapReq.java
@@ -1,140 +1,140 @@
-package io.odysz.jsample.cheap;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import io.odysz.semantic.jprotocol.AnsonBody;
-import io.odysz.semantic.jprotocol.AnsonMsg;
-import io.odysz.semantic.jserv.R.AnQueryReq;
-import io.odysz.semantic.jserv.U.AnUpdate;
-import io.odysz.semantic.jserv.U.AnUpdateReq;
-import io.odysz.semantics.IUser;
-import io.odysz.sworkflow.EnginDesign.Req;
-import io.odysz.transact.sql.Statement;
-import io.odysz.transact.x.TransException;
-
-public class CheapReq extends AnsonBody {
-	public static CheapReq format(AnsonMsg<AnQueryReq> parent, Req req, String wfId) {
-		CheapReq r = new CheapReq(parent);
-		r.a = req.name();
-		r.wftype = wfId;
-		return r;
-	}
-
-	protected String wftype;
-	public String wftype() { return wftype; }
-	public CheapReq wftype(String wfid) {
-		wftype = wfid;
-		return this;
-	}
-
-	protected HashMap<String,Object> cmdArgs;
-	public Object args(String n) { return cmdArgs == null ? null : cmdArgs.get(n); }
-
-	private CheapReq args(String n, String v) {
-		if (cmdArgs == null)
-			cmdArgs = new HashMap<String, Object>();
-		cmdArgs.put(n, v);
-		return this;
-	}
-	
-	public CheapReq taskId(String tid) { return args("taskId", tid); }
-	public String taskId() { return (String) args("taskId"); }
-	
-	public CheapReq nodeId(String nid) { return args("nodeId", nid); }
-	public String nodeId() { return (String) args("nodeId"); }
-	
-	public CheapReq usrId(String uid) { return args("usrId", uid); }
-	public String usrId() { return (String) args("usrId"); }
-
-	public CheapReq instId(String iid) { return args("instId", iid); }
-	public String instId() { return (String) args("instId"); }
-
-	protected String ndescpt;
-	public CheapReq nodeDesc(String descpt) {
-		this.ndescpt = descpt;
-		return this;
-	}
-
-	protected ArrayList<Object[]> taskNvs;
-	public ArrayList<Object[]> taskNvs() { return taskNvs; }
-	public CheapReq taskNv(String n, Object v) {
-		if (taskNvs == null)
-			taskNvs = new ArrayList<Object[]>();
-			
-		taskNvs.add(new Object[] {n, v});
-		return this;
-	}
-
-	protected String cmd;
-	public String cmd() { return cmd; }
-	
-	/** 3d array of post insertings */
-	protected ArrayList<AnUpdateReq> postUpds;
-	public CheapReq post(AnUpdateReq p) {
-		if (postUpds == null)
-			postUpds = new ArrayList<AnUpdateReq>();
-		postUpds.add(p);
-		return this;
-	}
-
-	public CheapReq(AnsonMsg<? extends AnsonBody> parent) {
-		super(parent, null); // client can't control engine's connection
-	}
-
-	/**This should used only by JHelper, fake is ignored.
-	 * @param parent
-	 * @param fake
-	 */
-	public CheapReq(AnsonMsg<? extends AnsonBody> parent, String fake) {
-		super(parent, null); // client can't control engine's connection
-	}
-
-	public String req() { return a; }
-
-	/**calling super.a(req)
-	 * @param req
-	 * @return this
-	 */
-	public CheapReq req(Req req) {
-		return (CheapReq) a(req.name());
-	}
-	
-	/**Ask Req.cmd with cmd name as parameter.
-	 * @param cmd
-	 * @return the req object
-	 */
-	public CheapReq reqCmd(String cmd) {
-		// return args("req", cmd);
-		this.cmd = cmd;
-		return this;
-	}
-
-	/**Ask the node's right.
-	 * @param nodeId
-	 * @param usrId
-	 * @param taskId
-	 * @return the req object
-	 */
-	public CheapReq cmdsRight(String nodeId, String usrId, String taskId) {
-		nodeId(nodeId);
-		usrId(usrId);
-		taskId(taskId);
-		return req(Req.rights);
-	}
-
-	public CheapReq loadFlow(String wfId, String taskId) {
-		this.wftype(wfId);
-		taskId(taskId);
-		return req(Req.load);
-	}
-
-	/**format post children delete, inserts, updates by users
-	 * 
-	 * @return update / delete / insert
-	 * @throws TransException 
-	 */
-	public ArrayList<Statement<?>> posts(IUser usr) throws TransException {
-		return AnUpdate.postUpds(postUpds, usr);
-	}
-}
+//package io.odysz.jsample.cheap;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//import io.odysz.semantic.jprotocol.AnsonBody;
+//import io.odysz.semantic.jprotocol.AnsonMsg;
+//import io.odysz.semantic.jserv.R.AnQueryReq;
+//import io.odysz.semantic.jserv.U.AnUpdate;
+//import io.odysz.semantic.jserv.U.AnUpdateReq;
+//import io.odysz.semantics.IUser;
+//import io.odysz.sworkflow.EnginDesign.Req;
+//import io.odysz.transact.sql.Statement;
+//import io.odysz.transact.x.TransException;
+//
+//public class CheapReq extends AnsonBody {
+//	public static CheapReq format(AnsonMsg<AnQueryReq> parent, Req req, String wfId) {
+//		CheapReq r = new CheapReq(parent);
+//		r.a = req.name();
+//		r.wftype = wfId;
+//		return r;
+//	}
+//
+//	protected String wftype;
+//	public String wftype() { return wftype; }
+//	public CheapReq wftype(String wfid) {
+//		wftype = wfid;
+//		return this;
+//	}
+//
+//	protected HashMap<String,Object> cmdArgs;
+//	public Object args(String n) { return cmdArgs == null ? null : cmdArgs.get(n); }
+//
+//	private CheapReq args(String n, String v) {
+//		if (cmdArgs == null)
+//			cmdArgs = new HashMap<String, Object>();
+//		cmdArgs.put(n, v);
+//		return this;
+//	}
+//	
+//	public CheapReq taskId(String tid) { return args("taskId", tid); }
+//	public String taskId() { return (String) args("taskId"); }
+//	
+//	public CheapReq nodeId(String nid) { return args("nodeId", nid); }
+//	public String nodeId() { return (String) args("nodeId"); }
+//	
+//	public CheapReq usrId(String uid) { return args("usrId", uid); }
+//	public String usrId() { return (String) args("usrId"); }
+//
+//	public CheapReq instId(String iid) { return args("instId", iid); }
+//	public String instId() { return (String) args("instId"); }
+//
+//	protected String ndescpt;
+//	public CheapReq nodeDesc(String descpt) {
+//		this.ndescpt = descpt;
+//		return this;
+//	}
+//
+//	protected ArrayList<Object[]> taskNvs;
+//	public ArrayList<Object[]> taskNvs() { return taskNvs; }
+//	public CheapReq taskNv(String n, Object v) {
+//		if (taskNvs == null)
+//			taskNvs = new ArrayList<Object[]>();
+//			
+//		taskNvs.add(new Object[] {n, v});
+//		return this;
+//	}
+//
+//	protected String cmd;
+//	public String cmd() { return cmd; }
+//	
+//	/** 3d array of post insertings */
+//	protected ArrayList<AnUpdateReq> postUpds;
+//	public CheapReq post(AnUpdateReq p) {
+//		if (postUpds == null)
+//			postUpds = new ArrayList<AnUpdateReq>();
+//		postUpds.add(p);
+//		return this;
+//	}
+//
+//	public CheapReq(AnsonMsg<? extends AnsonBody> parent) {
+//		super(parent, null); // client can't control engine's connection
+//	}
+//
+//	/**This should used only by JHelper, fake is ignored.
+//	 * @param parent
+//	 * @param fake
+//	 */
+//	public CheapReq(AnsonMsg<? extends AnsonBody> parent, String fake) {
+//		super(parent, null); // client can't control engine's connection
+//	}
+//
+//	public String req() { return a; }
+//
+//	/**calling super.a(req)
+//	 * @param req
+//	 * @return this
+//	 */
+//	public CheapReq req(Req req) {
+//		return (CheapReq) a(req.name());
+//	}
+//	
+//	/**Ask Req.cmd with cmd name as parameter.
+//	 * @param cmd
+//	 * @return the req object
+//	 */
+//	public CheapReq reqCmd(String cmd) {
+//		// return args("req", cmd);
+//		this.cmd = cmd;
+//		return this;
+//	}
+//
+//	/**Ask the node's right.
+//	 * @param nodeId
+//	 * @param usrId
+//	 * @param taskId
+//	 * @return the req object
+//	 */
+//	public CheapReq cmdsRight(String nodeId, String usrId, String taskId) {
+//		nodeId(nodeId);
+//		usrId(usrId);
+//		taskId(taskId);
+//		return req(Req.rights);
+//	}
+//
+//	public CheapReq loadFlow(String wfId, String taskId) {
+//		this.wftype(wfId);
+//		taskId(taskId);
+//		return req(Req.load);
+//	}
+//
+//	/**format post children delete, inserts, updates by users
+//	 * 
+//	 * @return update / delete / insert
+//	 * @throws TransException 
+//	 */
+//	public ArrayList<Statement<?>> posts(IUser usr) throws TransException {
+//		return AnUpdate.postUpds(postUpds, usr);
+//	}
+//}
--- jserv-sample/src/main/java/io/odysz/jsample/cheap/CheapServ.java
+++ jserv-sample/src/main/java/io/odysz/jsample/cheap/CheapServ.java
@@ -1,239 +1,239 @@
-package io.odysz.jsample.cheap;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io_odysz.FilenameUtils;
-import org.xml.sax.SAXException;
-
-import io.odysz.common.Utils;
-import io.odysz.jsample.protocol.Samport;
-import io.odysz.jsample.utils.SampleFlags;
-import io.odysz.semantic.jprotocol.AnsonMsg;
-import io.odysz.semantic.jprotocol.AnsonMsg.MsgCode;
-import io.odysz.semantic.jprotocol.AnsonResp;
-import io.odysz.semantic.jserv.JSingleton;
-import io.odysz.semantic.jserv.ServPort;
-import io.odysz.semantic.jserv.helper.Html;
-import io.odysz.semantic.jserv.x.SsException;
-import io.odysz.semantics.IUser;
-import io.odysz.semantics.x.SemanticException;
-import io.odysz.sworkflow.CheapApi;
-import io.odysz.sworkflow.CheapEnginv1;
-import io.odysz.sworkflow.CheapEvent;
-import io.odysz.sworkflow.CheapException;
-import io.odysz.sworkflow.CheapResp;
-import io.odysz.sworkflow.EnginDesign.Req;
-import io.odysz.sworkflow.ICheapEventHandler;
-import io.odysz.transact.sql.Statement;
-import io.odysz.transact.x.TransException;
-
-@WebServlet(description = "Handling work flow request", urlPatterns = { "/cheapflow.sample" })
-public class CheapServ extends ServPort<CheapReq> {
-	public static class WfProtocol {
-		public static String reqBody = "wfreq";
-
-		public static String ok = "ok";
-
-		static final String wfid = "wfid";
-		static final String cmd = "cmd";
-		static final String busid = "busid";
-		static final String current = "current";
-		static final String ndesc = "nodeDesc";
-		static final String nvs = "nvs";
-		static final String busiMulti = "multi";
-		static final String busiPostupdate = "post";
-		
-		static final String routes = "routes";
-
-		static final String wfName = "wfname";
-		static final String wfnode1 = "node1";
-		static final String bTabl = "btabl";
-	}
-
-	private static final long serialVersionUID = 1L;
-	private static final boolean logFlag = SampleFlags.cheapflow;
-
-	public CheapServ() {
-		super(null);
-		p = Samport.cheapflow;
-	}
-
-	static {
-		// Because of the java enum limitation, or maybe the author's knowledge limitation, 
-		// AnsonMsg needing a IPort instance to handle ports that implemented a new version of valof() method handling all ports.<br>
-		// E.g. {@link Samport#menu#valof(name)} can handling both {@link Port} and Samport's enums.
-		//
-		// If the same in SysMenu is surely called before this servlet going to work, this line can be comment out. 
-		AnsonMsg.understandPorts(Samport.cheapflow);
-	}
-
-	@Override
-	protected void onGet(AnsonMsg<CheapReq> jmsg, HttpServletResponse resp) throws IOException {
-		if (logFlag)
-			Utils.logi("---------- cheapflow.sample get ----------");
-
-		try {
-			// String t = req.getParameter("t");
-			String a = jmsg.body(0).a();
-			if ("reload-cheap".equals(a)) {
-				try {
-					CheapEnginv1.initCheap(FilenameUtils.concat(JSingleton.rootINF(), CheapEnginv1.confpath), null);
-					resp.getWriter().write(Html.ok("cheap reloaded"));
-				} catch (SAXException e) {
-					e.printStackTrace();
-					resp.getWriter().write(Html.err(e.getMessage()));
-				}
-				return;
-			}
-
-//			AnsonMsg<CheapReq> jmsg = ServletAdapter.<CheapReq>read(req, jcheapReq, CheapReq.class);
-			IUser usr = verifier.verify(jmsg.header());
-
-			CheapReq jreq = jmsg.body(0);
-			CheapResp cheap = handle(Req.parse(a), jreq, usr);
-			AnsonMsg<AnsonResp> rs = ok(cheap.rs());
-			write(resp, rs);
-		} catch (SemanticException e) {
-			write(resp, err(MsgCode.exSemantic, e.getMessage()));
-		} catch (SQLException e) {
-			e.printStackTrace();
-			write(resp, err(MsgCode.exTransct, e.getMessage()));
-		} catch (SsException e) {
-			if (logFlag)
-				e.printStackTrace();
-			write(resp, err(MsgCode.exSession, e.getMessage()));
-		} catch (TransException e) {
-			if (logFlag)
-				e.printStackTrace();
-			write(resp, err(MsgCode.ext, e.getMessage()));
-		} finally {
-			resp.flushBuffer();
-		}
-	}
-
-	@Override
-	protected void onPost(AnsonMsg<CheapReq> jmsg, HttpServletResponse resp) throws IOException {
-		if (logFlag)
-			Utils.logi("========== cheapflow.sample post ==========");
-
-		resp.setCharacterEncoding("UTF-8");
-		try {
-			// AnsonMsg<CheapReq> jmsg = ServletAdapter.<CheapReq>read(req, jcheapReq, CheapReq.class);
-			IUser usr = verifier.verify(jmsg.header());
-
-			CheapReq jreq = jmsg.body(0);
-			String a = jreq.req();
-
-			CheapResp cheap = handle(Req.parse(a), jreq, usr);
-			AnsonMsg<AnsonResp> rs = ok(cheap.rs());
-			write(resp, rs);
-		} catch (CheapException e) {
-			write(resp, err(MsgCode.ext, e.getMessage()));
-		} catch (SemanticException e) {
-			write(resp, err(MsgCode.exSemantic, e.getMessage()));
-		} catch (SQLException e) {
-			if (logFlag)
-				e.printStackTrace();
-			write(resp, err(MsgCode.exTransct, e.getMessage()));
-		} catch (SsException e) {
-			write(resp, err(MsgCode.exSession, e.getMessage()));
-		} catch (TransException e) {
-			if (logFlag)
-				e.printStackTrace();
-			write(resp, err(MsgCode.ext, e.getMessage()));
-		} finally {
-			resp.flushBuffer();
-		}
-	}
-
-	private CheapResp handle(Req req, CheapReq jobj, IUser usr) throws SQLException, TransException {
-		if (Req.start == req)
-			return start(jobj, usr);
-		else if (Req.cmd == req)
-			return cmd(jobj, usr);
-		else if (Req.rights == req) 
-			return right(jobj, usr);
-		else if (Req.load == req) 
-			return loadFlow(jobj, usr);
-		else if (Req.nodeCmds == req) 
-			return loadCmds(jobj, usr);
-		else throw new CheapException("Req(body.a) can not been handled: %s", req);
-	}
-
-	private CheapResp right(CheapReq jobj, IUser usr) throws SemanticException, SQLException {
-		String nid = jobj.nodeId();
-		String tid = jobj.taskId();
-		return CheapApi.right(jobj.wftype, usr.uid(), nid, tid);
-	}
-
-	private CheapResp loadFlow(CheapReq jobj, IUser usr) throws SQLException, TransException {
-		String wfid = jobj.wftype();
-		String tid = jobj.taskId();
-		return CheapApi.loadFlow(wfid, tid, usr);
-	}
-
-	private CheapResp loadCmds(CheapReq jobj, IUser usr) throws TransException, SQLException {
-		String wfid = jobj.wftype();
-		String nId = jobj.nodeId();
-		String iId = jobj.instId();
-		return CheapApi.loadCmds(wfid, nId, iId, usr.uid());
-	}
-	
-	private CheapResp start(CheapReq jobj, IUser usr) throws SQLException, TransException {
-		ArrayList<Statement<?>> postups = jobj.posts(usr);
-		CheapResp res = CheapApi.start(jobj.wftype, jobj.taskId())
-				.nodeDesc(jobj.ndescpt)
-				.taskNv(jobj.taskNvs)
-				// .taskChildMulti(insertabl, null, inserts)
-				.postupdates(postups)
-				.commitReq(usr.logAct("Start Flow " + jobj.wftype, "cheap.start"));
-
-		// simulating business layer handling events
-		// FIXME why events handling is here?
-		ICheapEventHandler eh = res.rmStepHandler();
-		if (eh != null) {
-			CheapEvent evt = (CheapEvent) res.event();
-			eh.onCmd(evt);
-		}
-
-		eh = (ICheapEventHandler) res.rmArriveHandler();
-		if (eh != null)
-			eh.onArrive(res.event());
-
-		return res;
-	}
-	
-	private CheapResp cmd(CheapReq jobj, IUser usr) throws SQLException, TransException {
-		String wftype = jobj.wftype();
-		String taskId = jobj.taskId();
-		String cmd = jobj.cmd();
-
-		ArrayList<Statement<?>> postups = jobj.posts(usr);
-		CheapResp res = CheapApi.next(wftype, taskId, cmd)
-				.nodeDesc(jobj.ndescpt)
-				// .taskChildMulti(jobj.childInsertabl, null, jobj.childInserts)
-				.postupdates(postups)
-				.commitReq(usr.logAct(String.format("Req %s - %s", jobj.wftype, cmd), "cheap.cmd"));
-
-		// FIXME why events handling is here?
-		// ICheapEventHandler eh = (ICheapEventHandler) res.remove("stepHandler");
-		ICheapEventHandler eh = res.rmStepHandler();
-
-		if (eh != null) {
-			// CheapEvent evt = (CheapEvent) res.get("evt");
-			eh.onCmd(res.event());
-		}
-
-		eh = res.rmArriveHandler();
-		if (eh != null)
-			eh.onArrive(res.event());
-
-		return res;
-	}
-
-}
+//package io.odysz.jsample.cheap;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.apache.commons.io_odysz.FilenameUtils;
+//import org.xml.sax.SAXException;
+//
+//import io.odysz.common.Utils;
+//import io.odysz.jsample.protocol.Samport;
+//import io.odysz.jsample.utils.SampleFlags;
+//import io.odysz.semantic.jprotocol.AnsonMsg;
+//import io.odysz.semantic.jprotocol.AnsonMsg.MsgCode;
+//import io.odysz.semantic.jprotocol.AnsonResp;
+//import io.odysz.semantic.jserv.JSingleton;
+//import io.odysz.semantic.jserv.ServPort;
+//import io.odysz.semantic.jserv.helper.Html;
+//import io.odysz.semantic.jserv.x.SsException;
+//import io.odysz.semantics.IUser;
+//import io.odysz.semantics.x.SemanticException;
+//import io.odysz.sworkflow.CheapApi;
+//import io.odysz.sworkflow.CheapEnginv1;
+//import io.odysz.sworkflow.CheapEvent;
+//import io.odysz.sworkflow.CheapException;
+//import io.odysz.sworkflow.CheapResp;
+//import io.odysz.sworkflow.EnginDesign.Req;
+//import io.odysz.sworkflow.ICheapEventHandler;
+//import io.odysz.transact.sql.Statement;
+//import io.odysz.transact.x.TransException;
+//
+//@WebServlet(description = "Handling work flow request", urlPatterns = { "/cheapflow.sample" })
+//public class CheapServ extends ServPort<CheapReq> {
+//	public static class WfProtocol {
+//		public static String reqBody = "wfreq";
+//
+//		public static String ok = "ok";
+//
+//		static final String wfid = "wfid";
+//		static final String cmd = "cmd";
+//		static final String busid = "busid";
+//		static final String current = "current";
+//		static final String ndesc = "nodeDesc";
+//		static final String nvs = "nvs";
+//		static final String busiMulti = "multi";
+//		static final String busiPostupdate = "post";
+//		
+//		static final String routes = "routes";
+//
+//		static final String wfName = "wfname";
+//		static final String wfnode1 = "node1";
+//		static final String bTabl = "btabl";
+//	}
+//
+//	private static final long serialVersionUID = 1L;
+//	private static final boolean logFlag = SampleFlags.cheapflow;
+//
+//	public CheapServ() {
+//		super(null);
+//		p = Samport.cheapflow;
+//	}
+//
+//	static {
+//		// Because of the java enum limitation, or maybe the author's knowledge limitation, 
+//		// AnsonMsg needing a IPort instance to handle ports that implemented a new version of valof() method handling all ports.<br>
+//		// E.g. {@link Samport#menu#valof(name)} can handling both {@link Port} and Samport's enums.
+//		//
+//		// If the same in SysMenu is surely called before this servlet going to work, this line can be comment out. 
+//		AnsonMsg.understandPorts(Samport.cheapflow);
+//	}
+//
+//	@Override
+//	protected void onGet(AnsonMsg<CheapReq> jmsg, HttpServletResponse resp) throws IOException {
+//		if (logFlag)
+//			Utils.logi("---------- cheapflow.sample get ----------");
+//
+//		try {
+//			// String t = req.getParameter("t");
+//			String a = jmsg.body(0).a();
+//			if ("reload-cheap".equals(a)) {
+//				try {
+//					CheapEnginv1.initCheap(FilenameUtils.concat(JSingleton.rootINF(), CheapEnginv1.confpath), null);
+//					resp.getWriter().write(Html.ok("cheap reloaded"));
+//				} catch (SAXException e) {
+//					e.printStackTrace();
+//					resp.getWriter().write(Html.err(e.getMessage()));
+//				}
+//				return;
+//			}
+//
+////			AnsonMsg<CheapReq> jmsg = ServletAdapter.<CheapReq>read(req, jcheapReq, CheapReq.class);
+//			IUser usr = verifier.verify(jmsg.header());
+//
+//			CheapReq jreq = jmsg.body(0);
+//			CheapResp cheap = handle(Req.parse(a), jreq, usr);
+//			AnsonMsg<AnsonResp> rs = ok(cheap.rs());
+//			write(resp, rs);
+//		} catch (SemanticException e) {
+//			write(resp, err(MsgCode.exSemantic, e.getMessage()));
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			write(resp, err(MsgCode.exTransct, e.getMessage()));
+//		} catch (SsException e) {
+//			if (logFlag)
+//				e.printStackTrace();
+//			write(resp, err(MsgCode.exSession, e.getMessage()));
+//		} catch (TransException e) {
+//			if (logFlag)
+//				e.printStackTrace();
+//			write(resp, err(MsgCode.ext, e.getMessage()));
+//		} finally {
+//			resp.flushBuffer();
+//		}
+//	}
+//
+//	@Override
+//	protected void onPost(AnsonMsg<CheapReq> jmsg, HttpServletResponse resp) throws IOException {
+//		if (logFlag)
+//			Utils.logi("========== cheapflow.sample post ==========");
+//
+//		resp.setCharacterEncoding("UTF-8");
+//		try {
+//			// AnsonMsg<CheapReq> jmsg = ServletAdapter.<CheapReq>read(req, jcheapReq, CheapReq.class);
+//			IUser usr = verifier.verify(jmsg.header());
+//
+//			CheapReq jreq = jmsg.body(0);
+//			String a = jreq.req();
+//
+//			CheapResp cheap = handle(Req.parse(a), jreq, usr);
+//			AnsonMsg<AnsonResp> rs = ok(cheap.rs());
+//			write(resp, rs);
+//		} catch (CheapException e) {
+//			write(resp, err(MsgCode.ext, e.getMessage()));
+//		} catch (SemanticException e) {
+//			write(resp, err(MsgCode.exSemantic, e.getMessage()));
+//		} catch (SQLException e) {
+//			if (logFlag)
+//				e.printStackTrace();
+//			write(resp, err(MsgCode.exTransct, e.getMessage()));
+//		} catch (SsException e) {
+//			write(resp, err(MsgCode.exSession, e.getMessage()));
+//		} catch (TransException e) {
+//			if (logFlag)
+//				e.printStackTrace();
+//			write(resp, err(MsgCode.ext, e.getMessage()));
+//		} finally {
+//			resp.flushBuffer();
+//		}
+//	}
+//
+//	private CheapResp handle(Req req, CheapReq jobj, IUser usr) throws SQLException, TransException {
+//		if (Req.start == req)
+//			return start(jobj, usr);
+//		else if (Req.cmd == req)
+//			return cmd(jobj, usr);
+//		else if (Req.rights == req) 
+//			return right(jobj, usr);
+//		else if (Req.load == req) 
+//			return loadFlow(jobj, usr);
+//		else if (Req.nodeCmds == req) 
+//			return loadCmds(jobj, usr);
+//		else throw new CheapException("Req(body.a) can not been handled: %s", req);
+//	}
+//
+//	private CheapResp right(CheapReq jobj, IUser usr) throws SemanticException, SQLException {
+//		String nid = jobj.nodeId();
+//		String tid = jobj.taskId();
+//		return CheapApi.right(jobj.wftype, usr.uid(), nid, tid);
+//	}
+//
+//	private CheapResp loadFlow(CheapReq jobj, IUser usr) throws SQLException, TransException {
+//		String wfid = jobj.wftype();
+//		String tid = jobj.taskId();
+//		return CheapApi.loadFlow(wfid, tid, usr);
+//	}
+//
+//	private CheapResp loadCmds(CheapReq jobj, IUser usr) throws TransException, SQLException {
+//		String wfid = jobj.wftype();
+//		String nId = jobj.nodeId();
+//		String iId = jobj.instId();
+//		return CheapApi.loadCmds(wfid, nId, iId, usr.uid());
+//	}
+//	
+//	private CheapResp start(CheapReq jobj, IUser usr) throws SQLException, TransException {
+//		ArrayList<Statement<?>> postups = jobj.posts(usr);
+//		CheapResp res = CheapApi.start(jobj.wftype, jobj.taskId())
+//				.nodeDesc(jobj.ndescpt)
+//				.taskNv(jobj.taskNvs)
+//				// .taskChildMulti(insertabl, null, inserts)
+//				.postupdates(postups)
+//				.commitReq(usr.logAct("Start Flow " + jobj.wftype, "cheap.start"));
+//
+//		// simulating business layer handling events
+//		// FIXME why events handling is here?
+//		ICheapEventHandler eh = res.rmStepHandler();
+//		if (eh != null) {
+//			CheapEvent evt = (CheapEvent) res.event();
+//			eh.onCmd(evt);
+//		}
+//
+//		eh = (ICheapEventHandler) res.rmArriveHandler();
+//		if (eh != null)
+//			eh.onArrive(res.event());
+//
+//		return res;
+//	}
+//	
+//	private CheapResp cmd(CheapReq jobj, IUser usr) throws SQLException, TransException {
+//		String wftype = jobj.wftype();
+//		String taskId = jobj.taskId();
+//		String cmd = jobj.cmd();
+//
+//		ArrayList<Statement<?>> postups = jobj.posts(usr);
+//		CheapResp res = CheapApi.next(wftype, taskId, cmd)
+//				.nodeDesc(jobj.ndescpt)
+//				// .taskChildMulti(jobj.childInsertabl, null, jobj.childInserts)
+//				.postupdates(postups)
+//				.commitReq(usr.logAct(String.format("Req %s - %s", jobj.wftype, cmd), "cheap.cmd"));
+//
+//		// FIXME why events handling is here?
+//		// ICheapEventHandler eh = (ICheapEventHandler) res.remove("stepHandler");
+//		ICheapEventHandler eh = res.rmStepHandler();
+//
+//		if (eh != null) {
+//			// CheapEvent evt = (CheapEvent) res.get("evt");
+//			eh.onCmd(res.event());
+//		}
+//
+//		eh = res.rmArriveHandler();
+//		if (eh != null)
+//			eh.onArrive(res.event());
+//
+//		return res;
+//	}
+//
+//}
