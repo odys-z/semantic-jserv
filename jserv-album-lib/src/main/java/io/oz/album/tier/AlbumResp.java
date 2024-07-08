4433b82ca7e6136afa56cf8d5ab09318412944af
Ody Zelenskyy <odys.zhou@gmail.com>
2024-07-08 13:30:18 +0800

    .

--- jserv-album-lib/src/main/java/io/oz/album/tier/AlbumResp.java
+++ jserv-album-lib/src/main/java/io/oz/album/tier/AlbumResp.java
@@ -102,7 +102,7 @@ public class AlbumResp extends DocsResp {
 				collect = new Collect(rs);
 				cid = collect.cid;
 			}
-			collect.addPhoto(rs, new PhotoMeta(conn));
+			collect.addPhoto(rs, new PhotoMeta(conn, null, null));
 		}
 		if (collect != null)
 			collectRecords.add(collect);
