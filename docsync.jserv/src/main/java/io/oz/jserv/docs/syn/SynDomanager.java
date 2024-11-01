63abaec8ccfa5302ed7f71aafb57526d94b6b8ee
Ody Zhou <odys.zhou@gmail.com>
2024-11-01 16:48:41 -0400

    Try extending ExchangePersist as SyssionPeer...

--- docsync.jserv/src/main/java/io/oz/jserv/docs/syn/SynDomanager.java
+++ docsync.jserv/src/main/java/io/oz/jserv/docs/syn/SynDomanager.java
@@ -380,13 +380,6 @@ public class SynDomanager extends SyndomContext implements OnError {
 			throw new ExchangeException(ready, null,
 						"Session pool is null at %s", synode);
 		
-//		while (!lockx(robot)) {
-//			float wait = block.onlocked(synlocker);
-//			if (wait < 0)
-//				return this;
-//			Thread.sleep((long) (wait * 1000));
-//		}
-
 		new Thread(() -> { 
 		try { 
 			lockme(block);
