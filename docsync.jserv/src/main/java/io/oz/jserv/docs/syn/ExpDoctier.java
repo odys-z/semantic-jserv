3b04e05279ca98912bea17bb7e06a060e359d876
Ody zhou <odys.zhou@gmail.com>
2024-12-06 16:12:19 +0800

    refactor: move DocsException to anclient.java, which shared by Albumtier and docys.jserv.
     .-- albumtier
     .    |     |
     .    |     +-- anclient.java
     .    |     |    +--[Doclientier,x...]
     .    |     |    +--[SessionClient...]
     .    |     |    +--------------------------+
     .    |     |                               |
     ^    +-------- syndoclib (jserv-album-lib) |
    test  |     |    | (protocol: AlbumReq)     |
     .    |     |    +--------------------------+-- semantic.jserv
    jserv-alubm |
       |        |
       |        |
       +----- docysync (SynssionPeer ...)

--- docsync.jserv/src/main/java/io/oz/jserv/docs/syn/ExpDoctier.java
+++ docsync.jserv/src/main/java/io/oz/jserv/docs/syn/ExpDoctier.java
@@ -24,6 +24,7 @@ import org.xml.sax.SAXException;
 
 import io.odysz.anson.x.AnsonException;
 import io.odysz.common.Utils;
+import io.odysz.jclient.DocsException;
 import io.odysz.module.rs.AnResultset;
 import io.odysz.semantic.DATranscxt;
 import io.odysz.semantic.DA.Connects;
@@ -48,7 +49,6 @@ import io.odysz.semantics.IUser;
 import io.odysz.semantics.SemanticObject;
 import io.odysz.semantics.x.SemanticException;
 import io.odysz.transact.x.TransException;
-import io.oz.album.x.DocsException;
 import io.oz.jserv.docs.meta.DeviceTableMeta;
 
 /**
@@ -164,8 +164,8 @@ public class ExpDoctier extends ServPort<DocsReq> {
 			if (rsp != null) {
 				write(resp, ok(rsp.syndomain(domx.synode)));
 			}
-		} catch (DocsException e) {
-			write(resp, err(MsgCode.ext, e.ex().toBlock()));
+//		} catch (DocsException e) {
+//			write(resp, err(MsgCode.ext, e.ex().toBlock()));
 		} catch (SemanticException e) {
 			write(resp, err(MsgCode.exSemantic, e.getMessage()));
 		} catch (SQLException | TransException e) {
