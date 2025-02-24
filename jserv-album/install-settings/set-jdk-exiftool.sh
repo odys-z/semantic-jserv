e7085f9cae2d53520b6cb7d67c52b8d890e04158
Odys Zhou <odys.zhou@gmail.com>
2025-02-25 00:48:19 +0800

    deprecating bash install, moving zip to installer source ...

--- jserv-album/install-settings/set-jdk-exiftool.sh
+++
@@ -1,7 +0,0 @@
-echo "First download jdk to ~/jserv ."
-echo "An cd to ~/jserv, run"
-echo "wget https://exiftool.org/Image-ExifTool-13.18.tar.gz"
-echo "tar -xvf Image-ExifTool-13.18.tar.gz"
-echo "Check https://exiftool.org/index.html for the latest Exiftool version."
-
-export JAVA_HOME=~/jserv/jdk-17.0.12 && export PATH=~/jserv/jdk-17.0.12/bin:~/jserv/image-exiftool:$PATH && java -version && exiftool -ver
