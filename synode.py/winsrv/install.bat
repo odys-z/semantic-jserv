15eaf00dd661ec0dba01e5d65dea8f425063d67c
Odys Zhou <odys.zhou@gmail.com>
2025-03-30 11:08:34 +0800

    task: windows service ...

--- synode.py/winsrv/install.bat
+++ synode.py/winsrv/install.bat
@@ -1,18 +1,36 @@
-portfolio.exe //IS//portfolio-srv --Install=%cd%\portfolio-srv.exe ^
+set jar-ver=0.7.1
+
+echo "Portfolio 0.7, Synode %1"
+echo "Run this file from .. dir, e. g. winsrv/install.bat"
+set synodedir=%cd%
+set classpath=%synodedir%/bin/jserv-album-%jar-ver%.jar
+echo %classpath%
+
+cd winsrv
+portfolio-srv.exe //IS//Portfolio-synode --Install=%cd%\portfolio-srv.exe ^
 --ServiceUser LocalSystem ^
---Description="Portfolio 0.7.0 id={synode}" ^
+--Description="Portfolio 0.7, Synode %1" ^
 --Jvm=auto ^
---Classpath=%cd%/bin/jserv-album-0.7.0.jar ^
+--Classpath=%classpath% ^
 --Startup=auto ^
 --StartMode=jvm ^
 --StartClass=io.oz.syntier.serv.SynotierJettyApp ^
---StartMethod=main ^
+--StartMethod=jvmStart ^
+--JvmOptions=-Dfile.encoding=UTF-8;-Dstdout.encoding=UTF-8;-Dstderr.encoding=UTF-8 ^
 --StopMode=java ^
 --StopClass=io.oz.syntier.serv.SynotierJettyApp ^
---StopMethod=stopserv ^
+--StopMethod=jvmStop ^
 --StopParams=stop ^
 --LogPath=%cd%\logs ^
 --StdOutput=auto ^
 --StdError=auto
 
-portfolio.exe //ES//portfolio-srv
+echo "Finding service main class:"
+jar tf %synodedir%\bin\jserv-album-%jar-ver%.jar | findstr "SynotierJettyApp"
+
+echo "For convert unreadable log files:"
+echo "iconv -f GB2312 -t UTF-8 winsrv/logs/commons-daemon.2025-03-28.log > commons-daemon-utf8.log"
+
+portfolio-srv.exe //ES//portfolio-srv
+
+cd ..
--- synode.py/winsrv/uninstall.bat
+++ synode.py/winsrv/uninstall.bat
@@ -1,2 +1,4 @@
-portfolio-srv.exe //SS//portfolio-srv --StopTimeout=5
-portfolio-srv.exe //DS//portfolio-srv
+cd winsrv
+portfolio-srv.exe //SS//portfolio-synode --StopTimeout=5
+portfolio-srv.exe //DS//portfolio-synode
+cd ..
