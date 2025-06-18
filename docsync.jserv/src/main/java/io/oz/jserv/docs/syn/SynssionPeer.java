70d132d19739f8ccca14ba81f5b98be9f7f5efbb
Odys Zhou <odys.zhou@gmail.com>
2025-06-18 18:51:50 +0800

    exchange paging is not correct on X29 <-> Y201

--- docsync.jserv/src/main/java/io/oz/jserv/docs/syn/SynssionPeer.java
+++ docsync.jserv/src/main/java/io/oz/jserv/docs/syn/SynssionPeer.java
@@ -65,7 +65,7 @@ import io.oz.jserv.docs.syn.SyncReq.A;
  */
 public class SynssionPeer {
 
-	public static boolean testDisableAutoDocRef = false;
+	public static boolean testDisableAutoDocRef = true;
 
 	/** */
 	final String conn;
