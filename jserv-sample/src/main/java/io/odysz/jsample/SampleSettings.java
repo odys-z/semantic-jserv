920904101d716d7275f82ec50d0852612a1ff6b9
Ody Z <odys.zhou@gmail.com>
2025-07-23 17:48:58 +0800

    task: refactor anclient tests before merging back to master branch

--- jserv-sample/src/main/java/io/odysz/jsample/SampleSettings.java
+++ jserv-sample/src/main/java/io/odysz/jsample/SampleSettings.java
@@ -1,32 +1,41 @@
 package io.odysz.jsample;
 
+import static io.odysz.common.LangExt.f;
 import static io.odysz.common.Utils.logi;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import io.odysz.anson.Anson;
+import io.odysz.common.FilenameUtils;
 
 /**
  * @since 1.5.3
  */
-public class SampleSettings {
+public class SampleSettings extends Anson {
 
-	public static SampleSettings load(String webinf, String settings_json) {
-		// TODO Auto-generated method stub
-		return null;
+	public static SampleSettings load(String webinf, String settings_json)
+			throws FileNotFoundException, IOException {
+		return (SampleSettings) Anson.fromPath(FilenameUtils.concat(webinf, settings_json));
 	}
 
 	public String vol_name;
 	public String conn;
+	public String port;
 
-	public static SampleSettings check(String webinf, String settings_json, boolean verbose) {
+	public static SampleSettings check(String webinf, String settings_json, boolean verbose)
+			throws FileNotFoundException, IOException {
 
 		logi("Loading settings: %s", settings_json);
 		SampleSettings settings = SampleSettings.load(webinf, settings_json);
 		logi("%s:\n%s\n%s", settings_json, settings.vol_name, settings.toString());
 
 		logi("%s : %s", settings.vol_name, System.getProperty(settings.vol_name));
-		return null;
+		return settings;
 	}
 
-	public String jserv() {
-		return null;
+	String jserv() {
+		return f("http://localhost:%s", port);
 	}
 
 }
