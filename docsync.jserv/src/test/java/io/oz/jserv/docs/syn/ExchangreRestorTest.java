e734234625b9bc2aa2aa51776c95db8282e53b69
Ody Z <odys.zhou@gmail.com>
2025-10-31 19:02:58 +0800

    testing: breakpoint resume and deprecating SynssionPeer.update2peer() as restore request/reply doesn't match...

---
+++ docsync.jserv/src/test/java/io/oz/jserv/docs/syn/ExchangreRestorTest.java
@@ -0,0 +1,207 @@
+package io.oz.jserv.docs.syn;
+
+import static io.odysz.common.LangExt.musteqs;
+import static io.odysz.common.Utils.awaitAll;
+import static io.odysz.common.Utils.logi;
+import static io.odysz.common.Utils.touchFile;
+import static io.odysz.common.Utils.turngreen;
+import static io.odysz.common.Utils.turnred;
+import static io.oz.jserv.docs.syn.singleton.SynodetierJoinTest.errLog;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.odysz.common.FilenameUtils;
+import io.odysz.common.IAssert;
+import io.odysz.jclient.syn.IFileProvider;
+import io.odysz.semantic.tier.docs.DeviceTableMeta;
+import io.odysz.semantic.tier.docs.ExpSyncDoc;
+import io.odysz.semantic.tier.docs.ShareFlag;
+import io.oz.jserv.docs.AssertImpl;
+import io.oz.jserv.docs.syn.singleton.SynotierJettyApp;
+import io.oz.syn.Docheck;
+import io.oz.syn.SynodeMode;
+
+class ExchangreRestorTest {
+
+	private static final String zsu = "zsu";
+	private static final String admin = "admin";
+	private static final String _8964 = "8964";
+
+	static final String web_inf = "WEB-INF";
+	static final String webinf = "./src/main/webapp/" + web_inf;
+	static final String config_xml = "config.xml";
+
+	static final String backup_hub = "src/test/resources/exbreak-hub.backup.json";
+	static final String backup_prv = "src/test/resources/exbreak-prv.backup.json";
+	// static final String backup_mob = "src/test/resources/exbreak-mob.backup.json";
+	
+	boolean[] light = new boolean[] {false};
+	private static SynotierJettyApp jhub;
+	private static SynotierJettyApp jprv;
+
+	static String hubs = "settings.hub.json"; 
+	static String prvs = "settings.prv.json"; 
+	// static String mobs = "settings.mob.json"; 
+
+	static String hubpath = "settings.hub.json"; 
+	static String prvpath = "settings.prv.json"; 
+	// static String mobpath = "settings.mob.json"; 
+
+	static String vol_hub = "../../../test/resources/exbreak-vols/volume-hub";
+	static String vol_prv = "../../../test/resources/exbreak-vols/volume-prv";
+	// static String vol_mob = "vol-breaks/volume-mob";
+
+	static boolean[] boot_lights = new boolean[] { false, false };
+	static Thread thr_synodes;
+
+	static Dev dev_x;
+	static Dev dev_y;
+	static Docheck ck_hub;
+	static Docheck ck_prv;
+	static IAssert azert = new AssertImpl();
+
+	private static String[] dev_reses;
+
+	static T_PhotoMeta docm; // = new T_PhotoMeta("clientconn-x");
+	static DeviceTableMeta devm; // = new DeviceTableMeta("clientconn-x");
+
+	@BeforeAll
+	static void installSynodes() throws Exception {
+		System.setProperty("WEB-INF", "src/main/webapp/WEB-INF");
+
+		System.setProperty("VOLUME_HUB", vol_hub);
+		System.setProperty("VOLUME_PRV", vol_prv);
+		// System.setProperty("VOLUME_MOB", vol_mob);
+		
+		// settings.json
+		hubpath = FilenameUtils.rel2abs(webinf, hubs);
+		prvpath = FilenameUtils.rel2abs(webinf, prvs);
+		// mobpath = FilenameUtils.rel2abs(SynotierSettingsTest.webinf, mobs);
+
+		Files.copy(Paths.get(backup_hub), Paths.get(hubpath), StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(Paths.get(backup_prv), Paths.get(prvpath), StandardCopyOption.REPLACE_EXISTING);
+		// Files.copy(Paths.get(backup_mob), Paths.get(mobpath), StandardCopyOption.REPLACE_EXISTING);
+		
+		String[] vols = new String[] { vol_hub, vol_prv }; // , vol_mob
+		for (String p : vols) {
+			String doc_db = FilenameUtils.rel2abs(webinf, p, "doc-jserv.db"); 
+			Files.delete(Paths.get(doc_db));
+			touchFile(doc_db);
+
+			String main_db = FilenameUtils.rel2abs(webinf, p, "jserv-main.db");
+			Files.delete(Paths.get(main_db));
+			touchFile(main_db);
+		}
+		
+		dev_x = new Dev("sys-X", "dev-y", admin, _8964, "X-0", zsu,
+								"src/test/res/anclient.java/Amelia Anisovych.mp4");
+		dev_y = new Dev("sys-X", "dev-x", admin, _8964, "X-0", zsu,
+								"src/test/res/anclient.java/Amelia Anisovych.mp4");
+		
+		dev_reses = new String[] {
+				"src/test/res/anclient.java/Amelia Anisovych.mp4",
+				"src/test/res/anclient.java/3-birds.wav",
+				"src/test/res/anclient.java/2-ontario.gif",
+				"src/test/res/anclient.java/1-pdf.pdf",
+		};
+
+		docm = new T_PhotoMeta("clientconn-x");
+		devm = new DeviceTableMeta("clientconn-x");
+		
+		ck_hub = new Docheck(azert, zsu, jhub.syngleton().syncfg.synconn,
+							jhub.syngleton().domanager(zsu).synode,
+							SynodeMode.hub, 3, docm, devm, true);
+
+		ck_prv = new Docheck(azert, zsu, jprv.syngleton().syncfg.synconn,
+							jhub.syngleton().domanager(zsu).synode,
+							SynodeMode.peer, 3, docm, devm, true);
+	}
+	
+	@AfterAll
+	static void clean() throws IOException, InterruptedException {
+//		try {
+//			while(ck_prv.docs() < 4 && ck_hub.docs() < 4)
+//				Thread.sleep(1000);
+//		} catch (Exception e) { fail(e.getMessage()); }
+
+		Files.delete(Paths.get(hubpath));
+		Files.delete(Paths.get(prvpath));
+		// Files.delete(Paths.get(mobpath));
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	void testBreakResume() throws Exception {
+		final boolean[] lights = new boolean[2];
+//		logrst("[DoclientierTest] Starting synode-tiers", 0);
+
+		turnred(lights);
+		thr_synodes = new Thread(() -> {
+			try {
+				jhub = SynotierJettyApp._main(new String[] {hubs}, false);
+				((T_SynDomanager)jhub.syngleton().domnger0()).domUpdater((d, s, p, xp)->{
+					logi("%s->%s", s, p);
+					turngreen(lights, 0);
+				});
+				jprv = SynotierJettyApp._main(new String[] {prvs}, true);
+				((T_SynDomanager)jprv.syngleton().domnger0()).domUpdater((d, s, p, xp)->{
+					logi("%s->%s", s, p);
+					turngreen(lights, 0);
+					turngreen(lights, 1);
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}, "Hub & Prv by ExchangeBreakResumeTest");
+		thr_synodes.start();
+
+		Thread.sleep(2000);
+		musteqs(jhub.syngleton().syncfg.domain, zsu);
+		musteqs(jprv.syngleton().syncfg.domain, zsu);
+		
+		ExpSyncDoc dx = clientPush(dev_x, jprv, dev_reses);
+		ExpSyncDoc dy = clientPush(dev_y, jhub, dev_reses);
+		
+		awaitAll(lights, -1);
+
+		assertEquals(ck_hub.docs(), 4);
+		assertEquals(ck_prv.docs(), 4);
+
+		// must reach broken cases
+		T_SynDomanager m = (T_SynDomanager) jprv.syngleton().domnger0();
+		assertTrue(m.breakpoints[0]);
+		assertTrue(m.breakpoints[1]);
+		assertTrue(m.breakpoints[2]);
+	}
+
+	/////////////////// helpers
+	///
+	static ExpSyncDoc clientPush(Dev dev, SynotierJettyApp jet, String... reses) throws Exception {
+
+		dev.login(jet.jserv(), errLog);
+		dev.client.fileProvider(new IFileProvider() {});
+		
+		ExpSyncDoc xdoc = null; 
+		for (String res : reses) {
+			logi("client pushing: uid %s, device %s",
+					dev.client.client.ssInfo().uid(), dev.client.client.ssInfo().device);
+			logi(reses);
+
+			xdoc = DoclientierTest.videoUpByApp(dev.client, dev.device, res, docm.tbl, ShareFlag.publish);
+			assertEquals(dev.device.id, xdoc.device());
+			assertEquals(dev.res, xdoc.fullpath());
+
+			DoclientierTest.verifyPathsPage(dev.client, docm.tbl, res);
+		}
+		return xdoc;
+	}
+
+}
---
+++ docsync.jserv/src/test/java/io/oz/jserv/docs/syn/T_SynDomanager.java
@@ -0,0 +1,166 @@
+package io.oz.jserv.docs.syn;
+
+import static io.odysz.common.LangExt.eq;
+import static io.odysz.common.LangExt.isNull;
+import static io.odysz.common.LangExt.mustnoBlankAny;
+import static io.odysz.common.Utils.logT;
+import static io.oz.syn.ExessionAct.close;
+import static io.oz.syn.ExessionAct.deny;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import io.odysz.anson.AnsonException;
+import io.odysz.common.Utils;
+import io.odysz.semantic.jserv.x.SsException;
+import io.odysz.semantics.x.ExchangeException;
+import io.odysz.semantics.x.SemanticException;
+import io.odysz.transact.x.TransException;
+import io.oz.jserv.docs.syn.SyncReq.A;
+import io.oz.jserv.docs.syn.singleton.AppSettings;
+import io.oz.jserv.docs.syn.singleton.Syngleton;
+import io.oz.syn.DBSyntableBuilder;
+import io.oz.syn.ExchangeBlock;
+import io.oz.syn.registry.SynodeConfig;
+
+/**
+ * The testing version of {@link SynDomanager}.
+ * 
+ * Enable {@link #T_SynDomanager(Syngleton, SynodeConfig, AppSettings, boolean)}'s break0-4
+ * to test broken exchange resuming.
+ */
+public class T_SynDomanager extends SynDomanager {
+	int emulate_break0_4;
+	/**
+	 * [0: ending, 1: init, 2..4: following exchanges]
+	 */
+	final boolean[] breakpoints;
+
+	OnDomainUpdate onDomUpdate;
+
+	public T_SynDomanager(Syngleton syngleton, SynodeConfig c, AppSettings s, boolean break0_4)
+			throws Exception {
+		super(syngleton, c, s);
+		emulate_break0_4 = break0_4 ? 0 : -1;
+		breakpoints = new boolean[5];
+	}
+	
+	public T_SynDomanager domUpdater(OnDomainUpdate upd) {
+		this.onDomUpdate = upd;
+		return this;
+	}
+
+	@Override
+	public SynDomanager synUpdateDomain(SynssionPeer peer, OnDomainUpdate... onok)
+			throws AnsonException, SsException, IOException, TransException  {
+		if (emulate_break0_4 >= 0) {
+			logT(new Object(){}, "-X-X- [%s <- %s] break -X-X-", peer.peer, synode);
+			emulate_break0_4 = synUpdateDomx_break(peer, emulate_break0_4);
+			
+			if (onDomUpdate != null) onDomUpdate.ok(domain(), synode, peer.peer);
+			return this;
+		}
+		else return super.synUpdateDomain(peer, (d, s, per, persist) -> {
+			if (!isNull(onok))
+				onok[0].ok(d, s, per, persist);
+			if (onDomUpdate != null)
+				onDomUpdate.ok(d, s, per, persist);
+		});
+	}
+
+	/**
+	 * Similar to {@link SynssionPeer#synwith_peer(OnMutexLock)}, but break at breakpoint.
+	 * @param c the Syn-session client
+	 * @param breakpoint
+	 * @return next breakpoint
+	 * @throws SemanticException
+	 * @throws AnsonException
+	 * @throws SsException
+	 * @throws IOException
+	 * @throws TransException
+	 */
+	private int synUpdateDomx_break(SynssionPeer c, int breakpoint)
+			throws SemanticException, AnsonException, SsException, IOException, TransException {
+
+		int returnpoint = breakpoint < 4 ? breakpoint + 1 : -1;
+
+		if (!eq(c.peer, synode)) {
+			c.checkLogin(admin);
+			// peer.synwith_peer((lockby) -> Math.random());
+
+			mustnoBlankAny(c, domain());
+
+			try {
+				Utils.logi("Locking and starting thread on domain updating: %s : %s -> %s"
+						+ "\n=============================================================\n",
+						domain(), synode, c.peer);
+
+				lockme((u)-> Math.random());
+
+				ExchangeBlock reqb = c.exesrestore();
+				SyncResp rep = null;
+				if (reqb != null) {
+					rep = c.ex_lockpeer(c.peer, A.exrestore, reqb);
+
+					if (rep.exblock != null && rep.exblock.synact() != deny)
+						// onsynrestorRep(rep.exblock, rep.domain);
+						c.onsyninitRep(rep.exblock, rep.domain);
+				}
+				else {
+					reqb = c.exesinit();
+					rep = c.ex_lockpeer(c.peer, A.exinit, reqb);
+
+					if (rep.exblock != null && rep.exblock.synact() != deny) 
+						// on start reply
+						c.onsyninitRep(rep.exblock, rep.domain);
+				}
+
+				breakpoints[1] = true;
+				if (breakpoint == 1) return returnpoint;
+				
+				int exchanges = 0;
+				while (rep.synact() != close) {
+					ExchangeBlock exb = c.syncdb(rep.exblock);
+					rep = c.exespush(c.peer, A.exchange, exb);
+					if (rep == null)
+						throw new ExchangeException(exb.synact(), c.xp,
+								"Got null reply for exchange session. %s : %s -> %s",
+								domain(), synode, c);
+					
+					++exchanges;
+					if (breakpoint == exchanges) {
+						breakpoints[(exchanges % (breakpoints.length - 2)) + 2] = true;
+						return returnpoint;
+					}
+				}
+				
+				// close
+				reqb = c.synclose(rep.exblock);
+
+				if (breakpoint == 0) {
+					breakpoints[0] = true;
+					return returnpoint;
+				}
+
+				rep = c.exespush(c.peer, A.exclose, reqb);
+				
+				if (!SynssionPeer.testDisableAutoDocRef) {
+					DBSyntableBuilder tb = new DBSyntableBuilder(this);
+					c.resolveRef206Stream(tb);
+					c.pushDocRef2me(tb, c.peer);
+				}
+				else
+					Utils.warn("[%s : %s - SynssionPeer] Update to peer %s, auto-resolving doc-refs is disabled.",
+							synode, domain(), c.peer);
+
+			} catch (TransException | SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally { unlockme(); }
+		}
+		return returnpoint;
+	}
+
+}
