287fede55d5b54a5fdb7470cb900e2bc79fb717a
Odys Zhou <odys.zhou@gmail.com>
2026-05-04 19:36:41 +0800

    auto generating EchoReq2; TDD: anson.cmake/t09_expect_semantier...

--- semantic.jserv/src/main/java/io/odysz/semantic/jserv/echo/EchoReq.java
+++ semantic.jserv/src/main/java/io/odysz/semantic/jserv/echo/EchoReq.java
@@ -1,6 +1,7 @@
 package io.odysz.semantic.jserv.echo;
 
 
+import io.odysz.anson.AnsonCtor;
 import io.odysz.semantic.jprotocol.AnsonBody;
 import io.odysz.semantic.jprotocol.AnsonMsg;
 
@@ -14,7 +15,26 @@ public class EchoReq extends AnsonBody {
 		 */
 		public static final String inet = "inet";
 	}
+	
+	/**
+	 * Not used in Java
+	 */
+	String echo;
 
+	@AnsonCtor(base={"na"}, initialist="string echo = m")
+	public EchoReq(String m) {
+		super(null, null);
+		echo = m;
+	}
+
+	/**
+	 * JSON:
+	 * [[], [""]]
+	 * 
+	 * c++: 
+	 * EchoReq() : AnsonBody("", EchoReq::_type_) {}
+	 */
+	@AnsonCtor(base= {""} )
 	public EchoReq() {
 		super(null, null);
 	}
