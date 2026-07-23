c9f9afa2339658233867a4e3262cd66c5b46f8fc
Odys Zhou <odys.zhou@gmail.com>
2026-07-23 18:03:19 +0800

    Fix tests according to shared ASTs. Add doc: ctor semantics examples.

--- jserv-gen.py3/test/t_03_gen_anclient_camke_doctier.py
+++ jserv-gen.py3/test/t_03_gen_anclient_camke_doctier.py
@@ -6,6 +6,25 @@ from anson.io.odysz.anson import Anson
 from src.semantier_gen.io.oz.semanticpeer.generator2 import gen_peers
 from semanticshare.io.odysz.reflect import PeerSettings
 
+def replace_in_file(file_path: str, src: str, replace: str):
+    try:
+        # Read the file content
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read()
+
+        # Perform the replacement
+        updated_content = content.replace(src, replace)
+
+        # Write the updated content back to the file
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(updated_content)
+
+        print(f"Successfully updated '{file_path}'.")
+
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 class GenJservTest(TestCase):
 
@@ -14,6 +33,10 @@ class GenJservTest(TestCase):
         settings = cast(PeerSettings, Anson.from_file(testpath + '/settings/t_03-anclient.cmake-doctier.json'))
 
         gen_peers(settings, ast_folder=testpath + '/ast')
+
+        replace_in_file(settings.cpp_gen,
+                        "docsreq-test-case-body-invalid.ast.json",
+                        "docsreq.ast.json")
 
         with (open('../../anson.cmake/tests/expect/t_10_doctier.hpp', 'r') as e,
               open(settings.cpp_gen, 'r') as f):
